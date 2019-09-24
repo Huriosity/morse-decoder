494239bcc44d206d98f0421beaf73f9365095602
Huriosity <sasha_gur1999@mail.ru>
2019-09-24 16:22:25 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,58 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let decodedString = "";
+    let length = expr.length;
+    if (length % 10 == 1) {
+        while (expr.length % 10 != 0) {
+            expr += "0";
+        }
+        length = expr.length;
+    }
+    let sizeString = length / 10;
+    let subStrArray = [];
+    for (let i = 0; i < sizeString; i++) {
+        subStrArray[i] = expr.substr(i * 10, 10);
+        subStrArray[i] = decodeSubStr(subStrArray[i]);
+        decodedString += checkWhithMorseTable(subStrArray[i]);
+    }
+    return decodedString;
+}
+
+function decodeSubStr(subStr) {
+
+    if (subStr[0] == '*') {
+        return "*";
+    }
+    let willReturn = '';
+    for (let i = 0; i < subStr.length; i++) {
+        if (subStr[i] == '0') {
+            continue;
+        }
+        if (subStr[i + 1] == '1') {
+            willReturn += '-';
+            i++;
+            continue;
+        }
+        else {
+            willReturn += '.';
+            i++;
+            continue;
+        }
+    }
+    return willReturn;
+}
+
+function checkWhithMorseTable(subStr) {
+    if (subStr == "*") {
+        return " ";
+    }
+    for (let code in MORSE_TABLE) {
+        if (code == subStr){
+            return MORSE_TABLE[code];
+        }
+    }
+    return "";
 }
 
 module.exports = {
